1fe94781ae85f6811dfcffad184ecd00db475769
noiru09 <neelviec@gmail.com>
2022-12-18 19:29:58 +0530

    firts commit js

---
+++ app.js
@@ -0,0 +1,3 @@
+var foodcost= prompt("What is the total amount?");
+document.write("The total food cost= ",foodcost,"<br>");
+document.write("Each one of you has to pay ",(foodcost/8).toFixed(2),"<br>");
---
+++ test.js
@@ -0,0 +1,13 @@
+/*document.write("This is an external js");
+document.getElementById('th').innerHTML="This is a para";*/
+var a1=5;
+var a2="This is me";
+var a3=4;
+document.write("5+4=",a1+a3,"<br>");//comma concatinates
+console.log("This is console");
+/*var age = prompt("Tell me your age");
+alert("your age is ",age);*/
+document.write("5-4=",a1-a3,"<br>");
+document.write("5*4=",a1*a3,"<br>");
+document.write("5/4=",a1/a3,"<br>");
+document.write("5%4=",a1%a3,"<br>");
